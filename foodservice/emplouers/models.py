a150a42fa659a56a2e281a31b3bbd2ab2ace426e
titoan88@gmail.com <titoan88@gmail.com>
2017-07-09 11:31:31 +0300

    change models in employers app

--- foodservice/emplouers/models.py
+++ foodservice/emplouers/models.py
@@ -4,23 +4,26 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class Emploue(models.Model):
+class Employers(models.Model):
     user = models.ForeignKey(User)
     name = models.CharField(max_length=120)
     phone = models.IntegerField(max_length=20)
-    location = models.CharField(max_length=300)
-    login_name = models.CharField(max_length=10)
+    location = models.CharField(max_length=300, null=True)
+    position = models.ForeignKey('Positions')
+    dismissed = models.BooleanField(default=False)
 
     class Meta:
-        db_table = 'emplouers'
+        db_table = 'employers'
 
     def as_dict(self):
         items = {
-            'user': self.user,
+            'user': self.user.username,
+            'user_id': self.user.id,
+            'employer_id': self.id,
             'name': self.name,
             'phone': self.phone,
             'location': self.location,
-            'login_name': self.login_name
+            'dismissed': self.dismissed,
         }
         return items
 
@@ -28,7 +31,7 @@ class Emploue(models.Model):
         return self.name
 
 
-class Admin(models.Model):
+class SiteAdmin(models.Model):
     user = models.ForeignKey(User)
     phone = models.IntegerField(max_length=20)
     name = models.CharField(max_length=50)
@@ -46,3 +49,13 @@ class Admin(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Positions(models.Model):
+    position_name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'positions'
+
+    def __str__(self):
+        return self.position_name
--- foodservice/emplouers/views.py
+++ foodservice/emplouers/views.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Employers
 
 # Create your views here.
+
+
+def all_employers(request):
+    if request.metod == 'GET':
+        employers = Employers.objects.all()
+    else:
+        args = {'error': True, 'message': 'Не допустимий тип запиту!'}
+        return JsonResponse(data=args, safe=False,  content_type='application/json', status=400)
